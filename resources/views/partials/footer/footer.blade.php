610c80cb0f998380935a8c76eb4ef070867fe1e2
Eric Landheer <e.landheer@mentechmedia.nl>
2018-01-29 16:29:35 +0100

    :bug: Footer

--- resources/views/partials/footer/footer.blade.php
+++ resources/views/partials/footer/footer.blade.php
@@ -2,49 +2,72 @@
     <div class="bg-main">
         <div class="container">
             <div class="row space-inside-md">
-                <div class="col-lg-5 col-md-5 col-xs-12 col-sm-5 xs-text-center sm-space-inside-sm xs-space-inside-sm text-color-secondary">
+                <div class="col-lg-3 col-md-5 col-xs-12 col-sm-5 xs-text-center sm-space-inside-sm xs-space-inside-sm text-color-secondary">
                     <h3 class="space-inside-down-sm text-color-light text-bold">Hosting</h3>
-                    <p class="text-bold text-color-accent">Pakketten</p>
-                    <ul>
-                        <li>Webhosting </li>
-                        <li>VPS </li>
-                        <li>Managed hosting </li>
-                    </ul>
-                    <p class="text-bold text-color-accent">Domeinen</p>
-                    <ul>
-                        <li>Domeinregistratie </li>
-                        <li>Spamfilter </li>
-                        <li>Beveiligingscheck/Pen Testing</li>
-                    </ul>
-                    <!-- <p class="text-bold text-color-accent">Telefonie</p>
-                    <ul>
-                        <li>Wat is VoIP?</li>
-                        <li>Mogelijkheden</li>
-                        <li>Kosten</li>
-                        <li>Referenties</li>
-                    </ul>
-                    <p class="text-bold text-color-accent">Online werken</p>
-                    <ul>
-                        <li>Office365</li>
-                        <li>Cloud storage</li>
-                    </ul> -->
+                    <div class="space-outside-down-lg">
+                        <p class="text-bold text-color-accent">Pakketten</p>
+                        <ul>
+                            <li>Webhosting </li>
+                            <li>VPS </li>
+                            <li>Managed hosting </li>
+                        </ul>
+                    </div>
+                    <div class="space-outside-down-lg">
+                        <p class="text-bold text-color-accent">Domeinen</p>
+                        <ul>
+                            <li>Domeinregistratie </li>
+                            <li>Spamfilter </li>
+                            <li>Beveiligingscheck/Pen Testing</li>
+                        </ul>
+                    </div>
+                    <div class="space-outside-down-lg">
+                        <p class="text-bold text-color-accent">Telefonie</p>
+                        <ul>
+                            <li>Wat is VoIP?</li>
+                            <li>Mogelijkheden</li>
+                            <li>Kosten</li>
+                            <li>Referenties</li>
+                        </ul>
+                    </div>
+                    <div class="space-outside-down-lg">
+                        <p class="text-bold text-color-accent">Online werken</p>
+                        <ul>
+                            <li>Office365</li>
+                            <li>Cloud storage</li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="col-xs-12 col-sm-7 col-md-4 xs-text-center sm-space-inside-sm xs-space-inside-sm text-color-secondary">
+                <div class="col-lg-3 col-md-4 col-sm-7 col-xs-12  xs-text-center sm-space-inside-sm xs-space-inside-sm text-color-secondary">
                     <h3 class="space-inside-down-sm text-color-light text-bold">Ontwikkeling</h3>
-                    <p class="text-bold text-color-accent">Applicatieontwikkeling</p>
-                    <ul>
-                        <li>Mogelijkheden </li>
-                        <li>Specialisatie </li>
-                        <li>Referenties </li>
-                    </ul>
-                    <p class="text-bold text-color-accent">Websites</p>
-                    <ul>
-                        <li>Mogelijkheden + CMS </li>
-                        <li>SEO </li>
-                        <li>Partners + referenties </li>
-                    </ul>
+                    <div class="space-outside-down-lg">
+                        <p class="text-bold text-color-accent">Applicatieontwikkeling</p>
+                        <ul>
+                            <li>Mogelijkheden </li>
+                            <li>Specialisatie </li>
+                            <li>Referenties </li>
+                        </ul>
+                    </div>
+                    <div class="space-outside-down-lg">
+                        <p class="text-bold text-color-accent">Websites</p>
+                        <ul>
+                            <li>Mogelijkheden + CMS </li>
+                            <li>SEO </li>
+                            <li>Partners + referenties </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="col-md-3 col-sm-12  col-xs-12 xs-text-center sm-space-inside-sm xs-space-inside-sm text-color-secondary">
+                <div class="col-lg-3 col-md-5 col-xs-12 col-sm-5 xs-text-center sm-space-inside-sm xs-space-inside-sm text-color-secondary">
+                    <h3 class="space-inside-down-sm text-color-light text-bold">Ondersteuning</h3>
+                    <div class="space-outside-down-lg">
+                        <p class="text-bold text-color-accent">Support</p>
+                        <ul>
+                            <li>Documentatie </li>
+                            <li>Storingen </li>
+                            <li>Vraag stellen </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 xs-text-center sm-space-inside-sm xs-space-inside-sm text-color-secondary">
                     <h3 class="space-inside-down-sm text-color-light text-bold">Hoofdkantoor</h3>
                     <p class="text-color-tertiary"><i class="material-icons space-inside-right-sm text-color-tertiary" style="position: relative; top: 7px;">pin_drop</i>Noorderhaven 46</p>
                     <p class="text-color-tertiary space-inside-xs"><i class="space-inside-right-lg"></i>9712 VL Groningen </p>
